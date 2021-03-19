7e911f5af49053ca8ab0f20ac81c747c46b32108
Abdurrahman Mohammad <abdurrahman.mohammad@sjsu.edu>
2021-03-18 23:21:26 -0700

    Part 2

--- Parstagram/SceneDelegate.swift
+++ Parstagram/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        NotificationCenter.default.addObserver(forName: Notification.Name("login"), object: nil, queue: OperationQueue.main) { (Notification) in
+            print("Logout notification received")
+            self.login()
+        }
+        
+        NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
+            print("Logout notification received")
+            self.login()
+        }
+        
+        if PFUser.current() != nil {
+            login()
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func login() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "FeedNavigationController")
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
